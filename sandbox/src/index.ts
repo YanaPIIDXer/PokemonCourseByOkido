f679bbb6c8e12d53ef31d296751c1ac0b1afca7f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-02 07:13:34 +0900

    ChatGPTの利用準備

--- sandbox/src/index.ts
+++ sandbox/src/index.ts
@@ -1,1 +1,15 @@
-console.log("ポケモン、ゲットだぜ！！");
+require("dotenv").config();
+
+import { PokemonClient } from "pokenode-ts";
+
+/**
+ * Entry Point
+ */
+const main = async (): Promise<void> => {
+  const client = new PokemonClient();
+  const result = await client.getPokemonById(1);
+  console.log(result.name);
+};
+
+// Start Program
+main();
