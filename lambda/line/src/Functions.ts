bb658991b5845130832316a46590a37968e07d58
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-11 18:20:19 +0900

    テンプレートをTypeScriptのソースコードにした

--- lambda/line/src/Functions.ts
+++ lambda/line/src/Functions.ts
@@ -1,7 +1,11 @@
 import { OpenAI } from "langchain/llms/openai";
 import { ConversationChain } from "langchain/chains";
 import { PromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate } from "langchain/prompts"
-import * as fs from "fs";
+import { template as translateTemplate } from "./templates/Translate";
+import { template as systemTemplate } from "./templates/System";
+import { template as descriptionTemplate } from "./templates/Description";
+import { template as punchLineTemplate } from "./templates/PunchLine";
+import { template as endTemplate } from "./templates/End";
 
 /**
  * 英語表記の名前から日本語表記の名前に変換
@@ -12,10 +16,9 @@ export const translateNameEngToJap = async (engName: string): Promise<string> =>
     modelName: "gpt-3.5-turbo",
   });
   
-  const templateText = fs.readFileSync("./templates/Translate.txt").toString();
   const template = new PromptTemplate({
     inputVariables: ["pokemon"],
-    template: templateText,
+    template: translateTemplate,
   });
   const chain = new ConversationChain({
     llm,
@@ -31,25 +34,22 @@ export const translateNameEngToJap = async (engName: string): Promise<string> =>
  * @param name ポケモンの名前（日本語表記）
  */
 export const fetchOkido = async (name: string): Promise<string> => {
-  const description = await fetchConversation("./templates/Description.txt", name);
-  const punchLine = await fetchConversation("./templates/PunchLine.txt", name);
-  const end = await fetchConversation("./templates/End.txt", name);
+  const description = await fetchConversation(descriptionTemplate, name);
+  const punchLine = await fetchConversation(punchLineTemplate, name);
+  const end = await fetchConversation(endTemplate, name);
   return description + "\n\n" + punchLine + "\n\n\n" + "ではここで一句。\n" + end;
 }
 
 /**
  * 会話
- * @param humanPromptTemplatePath HumanMessagePromptTemplateに渡すプロンプト
+ * @param humanTemplate HumanMessagePromptTemplateに渡すプロンプトテンプレート
  * @param pokemonName ポケモンの名前
  */
-const fetchConversation = async (humanPromptTemplatePath: string, pokemonName: string): Promise<string> => {
+const fetchConversation = async (humanTemplate: string, pokemonName: string): Promise<string> => {
   const llm = new OpenAI({
     modelName: "gpt-3.5-turbo",
     temperature: 0.5,
   });
-
-  const systemTemplate = fs.readFileSync("./templates/System.txt").toString();
-  const humanTemplate = fs.readFileSync(humanPromptTemplatePath).toString();
 
   const template = ChatPromptTemplate.fromPromptMessages([
     SystemMessagePromptTemplate.fromTemplate(systemTemplate),
